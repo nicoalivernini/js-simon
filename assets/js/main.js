a2626a8608b743be21ecf80f23b3188abb774832
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-15 15:46:23 +0100

    Richiesta numeri utente

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,22 @@
 //Array contente i numeri random
 var numeriRandom = [];
 
-// Creare 5 numeri casuali da 1 a 50
-for (var i = 0; i < 5; i++) {
-  //Richiamo la funzione e creo i numeri random
-  numeri = randomNumber(1, 50)
-  //Pusho i numeri dentro l'array
-  numeriRandom.push (numeri);
-}
-  //Seleziono il p in cui andrò ad inserire i numeri
-  $('ul').append('<li>' + numeriRandom + '</li>');
-  //Stampare i numeri dentro un p nel DOM
-
   //Mantenere visibili i numeri per 30 secondi
+  printNumber();
   //Alla fine dei 30 secondi nascondere i numeri
+
+
+
   //Pausa di 30 secondi
 
+//Salvo numeri utente dentro array
+var numeriUtente = [];
+console.log(numeriUtente);
 //Utente inserisce i numeri che ricorda attraverso il prompt
+for (var i = 0; i < 5; i++) {
+  var numeroUtente = parseInt(prompt('Inserisci un numero da 1 a 50'));
+  numeriUtente.push(numeroUtente);
+}
   //Stampo i numeri casuali
   //Stampo i numeri inseriti dall'Utente
   //Stampo i numeri ricordati e/o indovinati
@@ -25,8 +25,22 @@ for (var i = 0; i < 5; i++) {
 // FUNCTIONS
 
 //Numeri casuali
-
 function randomNumber(min, max) {
   var numero = Math.floor(Math.random() * (max) + min);
   return numero;
+}
+
+//Creazione numeri casuali e stampa su DOM
+
+function printNumber() {
+  // Creare 5 numeri casuali da 1 a 50
+  for (var i = 0; i < 5; i++) {
+    //Richiamo la funzione e creo i numeri random
+    numeri = randomNumber(1, 50)
+    //Pusho i numeri dentro l'array
+    numeriRandom.push(numeri);
+  }
+    //Seleziono il p in cui andrò ad inserire i numeri
+    //Stampare i numeri dentro un p nel DOM
+    $('ul').append('<li>' + numeriRandom + '</li>');
 }
