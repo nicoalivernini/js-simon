3d1999afc9bbfb6f4e9daf40433989125ed21ac9
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-15 17:51:30 +0100

    Aggiornamento ticket

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,37 @@
 //Array contente i numeri random
 var numeriRandom = [];
 
-  //Mantenere visibili i numeri per 30 secondi
-  printNumber();
-  //Alla fine dei 30 secondi nascondere i numeri
+//Creazione numeri casuali e stampa su DOM
+  // Creare 5 numeri casuali da 1 a 50
+  for (var i = 0; i < 5; i++) {
+    //Richiamo la funzione e creo i numeri random
+    numeri = randomNumber(1, 50)
+    //Pusho i numeri dentro l'array
+    numeriRandom.push(numeri);
+  }
+
+
+function printNumber() {
+  //Seleziono il p in cui andrò ad inserire i numeri
+  //Stampare i numeri dentro un p nel DOM
+  $('ul').append('<li>' + numeriRandom + '</li>');
+}
+
+printNumber();
+
+//Alla fine dei 30 secondi nascondere i numeri
+
+function hide() {
+  $('ul').append('<li>' + numeriRandom + '</li>').hide();
+}
+//Pausa di 30 secondi
+setTimeout(hide, 3000)
 
 
 
-  //Pausa di 30 secondi
 
+
+/*
 //Salvo numeri utente dentro array
 var numeriUtente = [];
 console.log(numeriUtente);
@@ -16,7 +39,8 @@ console.log(numeriUtente);
 for (var i = 0; i < 5; i++) {
   var numeroUtente = parseInt(prompt('Inserisci un numero da 1 a 50'));
   numeriUtente.push(numeroUtente);
-}
+}*/
+
   //Stampo i numeri casuali
   //Stampo i numeri inseriti dall'Utente
   //Stampo i numeri ricordati e/o indovinati
@@ -28,19 +52,4 @@ for (var i = 0; i < 5; i++) {
 function randomNumber(min, max) {
   var numero = Math.floor(Math.random() * (max) + min);
   return numero;
-}
-
-//Creazione numeri casuali e stampa su DOM
-
-function printNumber() {
-  // Creare 5 numeri casuali da 1 a 50
-  for (var i = 0; i < 5; i++) {
-    //Richiamo la funzione e creo i numeri random
-    numeri = randomNumber(1, 50)
-    //Pusho i numeri dentro l'array
-    numeriRandom.push(numeri);
-  }
-    //Seleziono il p in cui andrò ad inserire i numeri
-    //Stampare i numeri dentro un p nel DOM
-    $('ul').append('<li>' + numeriRandom + '</li>');
 }
