a68e3b4176bfe5a4b2f541d1f638cb101c87362d
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-15 15:24:51 +0100

    Creazione numeri casuali

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,32 @@
-console.log('ok');
+//Array contente i numeri random
+var numeriRandom = [];
+
+// Creare 5 numeri casuali da 1 a 50
+for (var i = 0; i < 5; i++) {
+  //Richiamo la funzione e creo i numeri random
+  numeri = randomNumber(1, 50)
+  //Pusho i numeri dentro l'array
+  numeriRandom.push (numeri);
+}
+  //Seleziono il p in cui andrò ad inserire i numeri
+  $('ul').append('<li>' + numeriRandom + '</li>');
+  //Stampare i numeri dentro un p nel DOM
+
+  //Mantenere visibili i numeri per 30 secondi
+  //Alla fine dei 30 secondi nascondere i numeri
+  //Pausa di 30 secondi
+
+//Utente inserisce i numeri che ricorda attraverso il prompt
+  //Stampo i numeri casuali
+  //Stampo i numeri inseriti dall'Utente
+  //Stampo i numeri ricordati e/o indovinati
+
+
+// FUNCTIONS
+
+//Numeri casuali
+
+function randomNumber(min, max) {
+  var numero = Math.floor(Math.random() * (max) + min);
+  return numero;
+}
